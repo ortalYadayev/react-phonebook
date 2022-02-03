dc0f066ceeaa00911b28139da2ec1eaa8096d156
Ortal Yadayev <ortalll20111@gmail.com>
2022-02-03 12:14:18 +0200

    add db.json

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instanceAxios = axios.create({
-    baseURL: 'http://localhost:3015/',
+    baseURL: 'http://localhost:3001/',
     headers: {
         'Accept': 'application/json',
     }
